eed48a2a856c6b72d4f4e117595d94d3cf719671
atome9797 <65073631+atome9797@users.noreply.github.com>
2022-09-27 17:11:14 +0900

    스크립트 수정본

--- Assets/Scripts/RankMenu.cs
+++ Assets/Scripts/RankMenu.cs
@@ -14,19 +14,12 @@ public class RankMenu : MonoBehaviour
     private GameObject poolingObjectPrefab;
 
 
-    private string SAVE_DATA_DIRECTORY; //저장할 폴더 경로
+    private string SAVE_DATA_DIRECTORY = "Assets/Resources/Save"; //저장할 폴더 경로
     private string SAVE_USER = "/SaveUser.txt"; //유저 파일
 
     // Start is called before the first frame update
     void Start()
     {
-        SAVE_DATA_DIRECTORY = Application.dataPath + "/Save/";
-
-        if (!Directory.Exists(SAVE_DATA_DIRECTORY)) //해당 경로가 존재하지 않는다면
-        {
-            Directory.CreateDirectory(SAVE_DATA_DIRECTORY);//폴더 생성(경로 생성)
-        }
-
         LoadDataUser();
     }
 
@@ -38,19 +31,28 @@ public class RankMenu : MonoBehaviour
             string loadJson = File.ReadAllText(SAVE_DATA_DIRECTORY + SAVE_USER);
             saveUser = JsonUtility.FromJson<SaveUser>(loadJson);
 
-            var newObjtemp = Instantiate(poolingObjectPrefab, transform).GetComponent<TextMeshProUGUI>();
-            newObjtemp.gameObject.SetActive(true);
-            newObjtemp.text = " ";
+            //상위 10위권 점수 불러오기
+            List<int> scorelist = new List<int>();
 
-            //유저 스코어 리스트 + 날짜 불러오기
             for (int i = 0; i < saveUser.UserScore.Count; i++)
             {
+                scorelist.Add(saveUser.UserScore[i] * (-1));
+            }
+
+            scorelist.Sort();
+
+            //순위 10위권 보이게 하기
+            for(int i = 0; i < scorelist.Count; i++)
+            {
+                if(i >= 10)
+                {
+                    break;
+                }
+
                 var newObj = Instantiate(poolingObjectPrefab, transform).GetComponent<TextMeshProUGUI>();
                 newObj.gameObject.SetActive(true);
-                newObj.text = saveUser.UserScore[i].ToString() + " 점 " + saveUser.UserScoreDate[i];
-                var newObj2 = Instantiate(poolingObjectPrefab, transform).GetComponent<TextMeshProUGUI>();
-                newObj2.gameObject.SetActive(true);
-                newObj2.text = " ";
+                int num = (-1) * scorelist[i];
+                newObj.text = $"{num} 점 ";
             }
 
         }
